b70c0f42968042f12d8dafcb3dee41b00439dbd3
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-10-24 15:07:18 +0200

    Feat(login) : Add login page and related components and types

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from "@/styles/components/Button.module.css";
+
+type Props = {
+  label: string;
+  onClickAction: () => void;
+  disabled?: boolean;
+}
+
+const Button = (props: Props) => {
+  const { label, onClickAction, disabled } = props
+
+  return (
+    <button
+      type={"submit"}
+      className={styles.button}
+      disabled={disabled}
+      onClick={onClickAction}
+    >
+      {label}
+    </button>
+  )
+}
+
+export default Button
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@fontsource/chivo"; // Defaults to weight 400
+import "@fontsource/chivo/400.css"; // Specify weight
+import "@fontsource/chivo/400-italic.css"; // Specify weight and style
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
